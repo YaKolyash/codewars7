394b5745a0baaf1b01554ac4ac1906aae71dbe46
Николай <99831971+YaKolyash@users.noreply.github.com>
2023-04-17 11:55:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Задание:
+Implement a function which convert the given boolean value into its string representation.
+
+    Note: Only valid inputs will be given.
+
+// Решение:
+
+function booleanToString(b) {
+    return b.toString();
+}
+
+
+
